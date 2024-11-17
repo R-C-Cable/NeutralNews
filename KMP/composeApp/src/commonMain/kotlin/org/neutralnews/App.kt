0a2e81eff7d289c162a741bf13efd1c83990b78b
Robert Cable <lrobertoc3@gmail.com>
2024-11-17 10:57:32 -0700

    adding top bar

--- KMP/composeApp/src/commonMain/kotlin/org/neutralnews/App.kt
+++ KMP/composeApp/src/commonMain/kotlin/org/neutralnews/App.kt
@@ -1,11 +1,16 @@
 package org.neutralnews
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.Navigator
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.core.context.startKoin
 import org.neutralnews.data.articles.articlesModule
 import org.neutralnews.data.articles.networkModule
+import org.neutralnews.ui.components.ScreenTopBar
 
 import org.neutralnews.ui.screens.newsfeed.NewsFeedScreen
 
@@ -25,5 +30,12 @@ fun App() {
         }
         koinInitialized = true;
     }
-    Navigator(NewsFeedScreen())
+
+    Navigator(NewsFeedScreen()) { navigator ->
+        Scaffold(topBar = { ScreenTopBar() }) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                navigator.lastItem.Content()
+            }
+        }
+    }
 }
