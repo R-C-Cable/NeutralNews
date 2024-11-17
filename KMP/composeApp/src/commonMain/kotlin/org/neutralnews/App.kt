5bf987ea0d5a7cbaa9d81cfdc9d52e06462c4890
Robert Cable <lrobertoc3@gmail.com>
2024-11-17 10:35:51 -0700

    adding temporary fix for starting koin

--- KMP/composeApp/src/commonMain/kotlin/org/neutralnews/App.kt
+++ KMP/composeApp/src/commonMain/kotlin/org/neutralnews/App.kt
@@ -9,14 +9,21 @@ import org.neutralnews.data.articles.networkModule
 
 import org.neutralnews.ui.screens.newsfeed.NewsFeedScreen
 
+//TODO implement a proper solution.
+// We should not need this boolean.
+var koinInitialized = false;
+
 @Composable
 @Preview
 fun App() {
-    startKoin {
-        modules(
-            networkModule,
-            articlesModule
-        )
+    if (!koinInitialized) {
+        startKoin {
+            modules(
+                networkModule,
+                articlesModule
+            )
+        }
+        koinInitialized = true;
     }
     Navigator(NewsFeedScreen())
 }
